12fe64952810381adc7ffd1dc75f7ca2129af246
Seokhoon Ha <lunaviv98@gmail.com>
2023-02-26 18:44:39 +0900

    init

---
+++ syntax/template.js
@@ -0,0 +1,7 @@
+const { log } = require("console");
+
+var name = 'John Doe';
+var letter = `Dear ${name}` 
+
+
+console.log(letter);
---
+++ syntax/variable2.js
@@ -0,0 +1,6 @@
+
+var name = 'john';
+
+var letter = 'Dear ' + name;
+
+console.log(letter);
